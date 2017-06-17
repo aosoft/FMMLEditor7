88598053722016f24742ef19c807fea8829202a0
aosoft <tan-y@aosoft.jp>
2017-06-18 02:15:30 +0900

    MML 解析対応中

---
+++ FMMLEditor7/FMPMMLAnalyzer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMMLEditor7
+{
+	struct FMPMMLAnalyzer
+	{
+		public string ADPCMPCMFile
+		{
+			get;
+			private set;
+		}
+
+		public string PPZPCMFile
+		{
+			get;
+			private set;
+		}
+
+		public bool UseOPNASpec
+		{
+			get;
+			private set;
+		}
+
+		public bool UseFM3Extend
+		{
+			get;
+			private set;
+		}
+
+		static public FMPMMLAnalyzer Analyze(string mmlPath)
+		{
+			var ret = new FMPMMLAnalyzer();
+
+			using (var sr = new StreamReader(mmlPath))
+			{
+				while (true)
+				{
+					var l = sr.ReadLine();
+					if (l == null)
+					{
+						break;
+					}
+
+					var splits = l.Trim().Split(' ', '\t');
+					if (splits.Length < 2 ||
+						splits[0][0] != '\'')
+					{
+						continue;
+					}
+
+					foreach (var c in splits[0])
+					{
+						switch (c)
+						{
+							case '$':
+								{
+									ret.PPZPCMFile = splits.Length == 2 ? splits[1] : null;
+								}
+								break;
+
+							case '#':
+								{
+									ret.ADPCMPCMFile = splits.Length == 2 ? splits[1] : null;
+								}
+								break;
+
+							case 'G':
+							case 'H':
+							case 'I':
+							case 'J':
+							case 'K':
+								{
+									ret.UseOPNASpec = true;
+								}
+								break;
+
+							case 'X':
+							case 'Y':
+							case 'Z':
+								{
+									ret.UseFM3Extend = true;
+								}
+								break;
+						}
+					}
+				}
+			}
+
+			return ret;
+		}
+	}
+}
---
+++ FMMLEditor7/PMDMMLAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMMLEditor7
+{
+	struct PMDMMLAnalyzer
+	{
+		public string CompiledFileName
+		{
+			get;
+			set;
+		}
+
+		static public PMDMMLAnalyzer Analyze(string mmlPath)
+		{
+			var ret = new PMDMMLAnalyzer();
+
+			using (var sr = new StreamReader(mmlPath))
+			{
+				while (true)
+				{
+					var l = sr.ReadLine();
+					if (l == null)
+					{
+						break;
+					}
+					var splits = l.Split(' ', '\t');
+
+					int count = splits.Length - 1;
+					for (int i = 0; i < count; i++)
+					{
+						if (splits[i] != null &&
+							splits[i].Equals("#Filename", StringComparison.OrdinalIgnoreCase))
+						{
+							ret.CompiledFileName = splits[i + 1];
+							break;
+						}
+					}
+				}
+			}
+
+			return ret;
+		}
+	}
+}
